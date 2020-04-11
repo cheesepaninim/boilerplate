42d63755640bf849cdaf13edcd4ebad0192e0562
cheesepaninim <hpw0310@gmail.com>
2020-04-12 04:14:38 +0900

    gatsby plugins

--- gatsby_mobx/gatsby-config.js
+++ gatsby_mobx/gatsby-config.js
@@ -1,8 +1,21 @@
+const mongodb = require("./_config/mongodb");
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby MobX starter',
   },
   plugins: [
     'gatsby-plugin-typescript',
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-mongodb`,
+      options: {
+        dbName: `${mongodb.dbName}`,
+        collection: `${mongodb.collection}`,
+        server: { address: `${mongodb.server.address}`, port: `${mongodb.server.port}` },
+        auth: { user: `${mongodb.auth.user}`, password: `${mongodb.auth.password}` },
+        map: { documents: { description: `${mongodb.map.documents.description}` } }
+      },
+    },
   ],
 };
