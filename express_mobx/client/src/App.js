88d91cef3dd93d12a5f6257b8744c8493fb058ac
cheesepaninim <hpw0310@gmail.com>
2020-04-16 01:17:47 +0900

    component 분리

--- express_mobx/client/src/App.js
+++ express_mobx/client/src/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react'
 import './App.css'
 
-import {inject, observer} from 'mobx-react'
+import Counter from './component/Counter'
 
-@inject('counter')
-// to get specific store
-// @inject(store => ({
-//   number: store.counter.number,
-//   increase: store.counter.increase,
-//   decrease: store.counter.decrease
-// }))
-@observer
 class App extends Component {
   render() {
-    const { counter } = this.props
+
     return (
         <div className="App">
-          <h1>{counter.number}</h1>
-          <button onClick={counter.increase}>+1</button>
-          <button onClick={counter.decrease}>-1</button>
+          {/* Counter with class */}
+          <Counter/>
         </div>
     )
   }
