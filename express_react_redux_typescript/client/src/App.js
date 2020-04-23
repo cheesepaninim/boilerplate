8f3984e84b13932f3a2190f08b23669918037728
cheesepaninim <hpw0310@gmail.com>
2020-04-23 22:59:36 +0900

    express, react default setup

---
+++ express_react_redux_typescript/client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const App = () => {
+  return (
+      <>
+        <div>Hello React!</div>
+        <div>with redux, react-helmet, typescript, express</div>
+
+        <div>Counter</div>
+      </>
+  )
+}
+
+export default App
---
+++ express_react_redux_typescript/client/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  mode: "development",
+
+  entry: "./src/index",
+
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      // index.html에 output에서 만들어진 bundle.js를 적용하여, dist에 새로운 html 파일 생성
+      template: `./public/index.html`
+    })
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),  // 해당 경로의 파일 변경시 번들
+    compress: true,
+    port: 80                                    // default: 8080
+  }
+
+}
